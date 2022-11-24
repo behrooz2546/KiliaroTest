98b38b66bfe0481a68162214740d659611865b15
behrooz azimifar <behrooz2546@gmail.com>
2022-11-24 12:58:22 +0330

    Initial Commit

---
+++ kiliaroTest/kiliaroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiliaroTestApp.swift
+//  kiliaroTest
+//
+//  Created by behrooz azimifar on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct kiliaroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
